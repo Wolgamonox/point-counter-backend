78516f2451ad4f44babb8aff978865ca1552ca10
Gaston Wolfart <gastonwolfart@gmail.com>
2024-06-14 23:45:49 +0200

    continued refactoring

--- src/game_session.rs
+++ src/game_session.rs
@@ -1,9 +1,13 @@
 use serde::{Deserialize, Serialize};
+use serial_int::SerialGenerator;
 
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex, OnceLock},
+};
 
 use tokio::{
-    net::TcpStream,
+    net::{TcpListener, TcpStream},
     sync::{broadcast, mpsc},
 };
 
@@ -12,11 +16,24 @@ use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
 use crate::game_model::GameState;
 
 /// The maximum number of player in a game
-pub const MAX_PLAYER_COUNT: usize = 10;
+const MAX_PLAYER_COUNT: usize = 10;
+
+/// The maximum number of games in the server
+const MAX_GAME_COUNT: usize = 16;
 
 // Types to communicate between the clients and a game
-pub type ClientSender = mpsc::Sender<ClientMessage>;
-pub type GameStateReceiver = broadcast::Receiver<GameState>;
+type ClientSender = mpsc::Sender<ClientMessage>;
+type GameStateReceiver = broadcast::Receiver<GameState>;
+
+static ID_GENERATOR: OnceLock<Mutex<SerialGenerator<u32>>> = OnceLock::new();
+
+fn generate_id() -> u32 {
+    ID_GENERATOR
+        .get_or_init(|| Mutex::new(SerialGenerator::<u32>::new()))
+        .lock()
+        .expect("A thread panicked while trying to generate an ID.")
+        .generate()
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ClientMessage {
@@ -25,6 +42,63 @@ pub enum ClientMessage {
         player_name: String,
         new_points: i32,
     },
+}
+
+pub async fn launch_game_session() {
+    let addr = "127.0.0.1:8080";
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await;
+    let listener = try_socket.expect("Failed to bind");
+    println!("Listening on: {}", addr);
+
+    // Define game state
+    let game_state = Arc::new(Mutex::new(GameState::new()));
+
+    // Define channels to communicate with players
+    let (game_state_tx, _game_state_rx) = broadcast::channel::<GameState>(MAX_GAME_COUNT);
+    let (client_tx, mut client_rx) = mpsc::channel::<ClientMessage>(MAX_PLAYER_COUNT);
+
+    let game_state = Arc::clone(&game_state);
+    let session_game_tx = game_state_tx.clone();
+
+    // Spawn a task that broadcasts a new gamestate whenever there is a point change
+    tokio::spawn(async move {
+        while let Some(client_msg) = client_rx.recv().await {
+            // Get game state
+            let mut game_state = game_state.lock().unwrap();
+
+            match client_msg {
+                ClientMessage::PlayerJoin(player_name) => {
+                    (*game_state).add_player(player_name);
+                }
+                ClientMessage::PointEvent {
+                    player_name,
+                    new_points,
+                } => {
+                    // Change points of player
+                    let player = (*game_state)
+                        .players
+                        .iter_mut()
+                        .find(|p| p.name == player_name);
+
+                    if let Some(player) = player {
+                        player.add_points(new_points);
+                    }
+                }
+            }
+
+            // Send updated game state down channel
+            session_game_tx.send(game_state.clone()).unwrap();
+        }
+    });
+
+    // Spawn a task for each client
+    while let Ok((stream, addr)) = listener.accept().await {
+        // Give the player the channels to communicate
+        let client_tx = client_tx.clone();
+        let game_state_rx = game_state_tx.subscribe();
+        tokio::spawn(handle_connection(stream, addr, client_tx, game_state_rx));
+    }
 }
 
 async fn process_client_msg(
@@ -64,7 +138,7 @@ async fn process_client_msg(
     }
 }
 
-pub async fn handle_connection(
+async fn handle_connection(
     raw_stream: TcpStream,
     addr: SocketAddr,
     client_tx: ClientSender,
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,11 @@
 pub mod game_model;
 pub mod game_session;
 
-use crate::game_model::GameState;
-use crate::game_session::{handle_connection, ClientMessage, MAX_PLAYER_COUNT};
+use crate::game_session::{launch_game_session, ClientMessage};
 
-use std::{
-    io,
-    sync::{Arc, Mutex},
-};
+use std::io;
 
-use tokio::{
-    net::TcpListener,
-    sync::{broadcast, mpsc},
-};
-
-/// The maximum number of games in the server
-const MAX_GAME_COUNT: usize = 16;
-
-async fn launch_game_session() {
-    let addr = "127.0.0.1:8080";
-    // Create the event loop and TCP listener we'll accept connections on.
-    let try_socket = TcpListener::bind(&addr).await;
-    let listener = try_socket.expect("Failed to bind");
-    println!("Listening on: {}", addr);
-
-    // Define game state
-    let game_state = Arc::new(Mutex::new(GameState::new()));
-
-    // Define channels to communicate with players
-    let (game_state_tx, _game_state_rx) = broadcast::channel::<GameState>(MAX_GAME_COUNT);
-    let (client_tx, mut client_rx) = mpsc::channel::<ClientMessage>(MAX_PLAYER_COUNT);
-
-    let game_state = Arc::clone(&game_state);
-    let session_game_tx = game_state_tx.clone();
-
-    // Spawn a task that broadcasts a new gamestate whenever there is a point change
-    tokio::spawn(async move {
-        while let Some(client_msg) = client_rx.recv().await {
-            // Get game state
-            let mut game_state = game_state.lock().unwrap();
-
-            match client_msg {
-                ClientMessage::PlayerJoin(player_name) => {
-                    (*game_state).add_player(player_name);
-                }
-                ClientMessage::PointEvent {
-                    player_name,
-                    new_points,
-                } => {
-                    // Change points of player
-                    let player = (*game_state)
-                        .players
-                        .iter_mut()
-                        .find(|p| p.name == player_name);
-
-                    if let Some(player) = player {
-                        player.add_points(new_points);
-                    }
-                }
-            }
-
-            // Send updated game state down channel
-            session_game_tx.send(game_state.clone()).unwrap();
-        }
-    });
-
-    // Spawn a task for each client
-    while let Ok((stream, addr)) = listener.accept().await {
-        // Give the player the channels to communicate
-        let client_tx = client_tx.clone();
-        let game_state_rx = game_state_tx.subscribe();
-        tokio::spawn(handle_connection(stream, addr, client_tx, game_state_rx));
-    }
-}
+use tokio::{net::TcpListener, sync::mpsc};
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
@@ -90,7 +23,7 @@ async fn main() -> Result<(), io::Error> {
         .unwrap()
     );
 
-    // Add code here to wait for a message before launching a game session
+    //TODO  Add code here to wait for a message before launching a game session
 
     // Launch a game session
     tokio::spawn(launch_game_session());
